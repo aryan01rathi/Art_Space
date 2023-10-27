4e0b9a1ef0ae1b0937b7ebb293422026187d7d72
Aryan Rathi <aryan2001rathi@gmail.com>
2023-10-28 01:05:12 +0530

    Next and Previous Button functionality

--- app/src/main/java/com/example/art_space/MainActivity.kt
+++ app/src/main/java/com/example/art_space/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,6 +29,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.art_space.ui.theme.Art_SpaceTheme
+import java.time.Year
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +43,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun LayoutBasic(modifier:Modifier=Modifier){
+fun LayoutBasic(){
+    var currentIndex by remember{ mutableStateOf(0) }
+
+    var artworks=listOf(
+        artspaceinfo(R.drawable.trial_image, R.string.title1, R.string.Artist1, R.string.year1),
+        artspaceinfo(R.drawable.surfboard, R.string.title2, R.string.Artist2, R.string.year2),
+        artspaceinfo(R.drawable.randomgirl, R.string.title3, R.string.Artist3, R.string.year3)
+    )
+
+    var currentArtwork=artworks[currentIndex]
     val backgroundGradient = Brush.linearGradient(
         colors = listOf(Color(0xFF7D7C7C), Color(0xFFB4B4B3)),
         start = Offset(100f, 300f),
@@ -54,16 +68,68 @@ fun LayoutBasic(modifier:Modifier=Modifier){
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            FirstPart()
-            SecondPart()
-            ThirdPart()
+            FirstPart(currentArtwork.artwork)
+            SecondPart(currentArtwork.title,currentArtwork.artist , currentArtwork.year)
+            ThirdPart(currentIndex,artworks.size){
+                newIndex->currentIndex=newIndex
+            }
         }
     }
 }
+data class artspaceinfo(
+    var artwork:Int,
+    var title:Int,
+    var artist:Int,
+    var year:Int
+)
+
+/*
 @Composable
-fun FirstPart(modifier:Modifier=Modifier){
+
+fun Change(count:Int,modifier: Modifier= Modifier,){
+    /*
+    var artworkImage by remember{ mutableStateOf(R.drawable.trial_image) }
+    var titleName by remember{ mutableStateOf(R.string.title) }
+    var artistName by remember{ mutableStateOf(R.string.Artist1) }
+    var year by remember{ mutableStateOf(R.string.year) }
+    */
+    when(count){
+        1->{
+            LayoutBasic(
+
+                R.drawable.trial_image,
+                R.string.title1,
+                R.string.Artist1,
+                R.string.year1,
+            )
+        }
+        2->{
+            LayoutBasic(
+
+            R.drawable.surfboard,
+            R.string.title2,
+            R.string.Artist2,
+            R.string.year2,
+            )
+        }
+        3->{
+            LayoutBasic(
+
+            R.drawable.randomgirl,
+            R.string.title3,
+            R.string.Artist3,
+            R.string.year3,
+            )
+        }
+    }
+}
+
+ */
+
+@Composable
+fun FirstPart(Artwork:Int, modifier: Modifier =Modifier){
         Image(
-            painter = painterResource(id = R.drawable.trial_image),
+            painter = painterResource(Artwork),
             contentDescription = "",
             modifier = Modifier
                 .padding(top = 10.dp)
@@ -75,18 +141,17 @@ fun FirstPart(modifier:Modifier=Modifier){
                 ),
             contentScale = ContentScale.Crop
         )
-
 }
-
 @Composable
-fun SecondPart(modifier:Modifier=Modifier){
+fun SecondPart(Title:Int, Artist: Int,Year: Int,modifier:Modifier=Modifier){
+
     Column(
         modifier = modifier.padding(10.dp), // Use modifier as the receiver for padding
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            stringResource(id = R.string.title),
+            stringResource(Title),
             modifier = Modifier.padding(5.dp) // Use Modifier here for Text
         )
         Divider(
@@ -95,21 +160,26 @@ fun SecondPart(modifier:Modifier=Modifier){
             modifier = Modifier.fillMaxWidth(0.4f) // Use Modifier here for Divider
         )
         Text(
-            stringResource(id = R.string.Artist1) + " ( " + stringResource(id = R.string.year) + " ) ",
+            stringResource(Artist) + " ( " + stringResource(Year) + " ) ",
             modifier = Modifier.padding(5.dp) // Use Modifier here for Text
         )
     }
 }
 
 @Composable
-fun ThirdPart(modifier: Modifier=Modifier){
+fun ThirdPart(currentIndex: Int, totalArtworks: Int,modifier: Modifier=Modifier,onIndexChanged: (Int) -> Unit){
+  // var correntartworkIndex by remember{ mutableStateOf(currentIndex) }
     Row(
         modifier.padding(10.dp),
         verticalAlignment = Alignment.Bottom,
         horizontalArrangement = Arrangement.SpaceEvenly
     ){
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                      if(currentIndex>0){
+                          onIndexChanged(currentIndex-1)
+                      }
+            },
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = colorResource(id = R.color.darkBlue)),
             modifier = modifier.size(150.dp,40.dp),
@@ -120,7 +190,11 @@ fun ThirdPart(modifier: Modifier=Modifier){
         }
         Spacer(modifier.padding(30.dp,3.dp,30.dp,0.dp))
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                if (currentIndex < totalArtworks - 1) {
+                    onIndexChanged(currentIndex + 1)
+                }
+                      },
             colors = ButtonDefaults.buttonColors(backgroundColor= colorResource(id=R.color.darkBlue)),
             modifier = modifier
                 .size(150.dp,40.dp),
@@ -131,12 +205,11 @@ fun ThirdPart(modifier: Modifier=Modifier){
             ){
             Text(stringResource(id = R.string.next))
         }
-
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun AppPreview(){
-    LayoutBasic()
+    LayoutBasic(  )
 }
